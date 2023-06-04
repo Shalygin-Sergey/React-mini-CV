91251a2e03fa3f3019ae9fb5cfcf68af94bd0508
Sergey <bkserega18@yandex.ru>
2023-06-04 14:42:28 +0300

    added detect mode window.matchMedia

--- src/components/buttonDarkMode/BtnDarkMode.js
+++ src/components/buttonDarkMode/BtnDarkMode.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useLocalStorage } from './../../utils/useLocalStorage';
+import detectDarkMode from './../../utils/detectDarkMode';
+
 import sun from './sun.svg';
 import moon from './moon.svg';
 import './style.css';
 
 const BtnDarkMode = () => {
 
-  const [darkMode, setDarkMode] = useLocalStorage('darkMode', 'light');
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode());
 
   const btnRef = React.useRef(null);
 
