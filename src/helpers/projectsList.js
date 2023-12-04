08bdd201807b35c39d91688e12126fe8ddd7e59e
sergeyshalygin <shalygin.javascript@gmail.com>
2023-12-04 23:12:52 +0300

    feat: закомментил заглушки

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -4,17 +4,17 @@ import project01Big from './../img/projects/01-big.jpg';
 import project02 from './../img/projects/02.jpg';
 import project02Big from './../img/projects/02-big.jpg';
 
-import project03 from './../img/projects/03.jpg';
-import project03Big from './../img/projects/03-big.jpg';
-
-import project04 from './../img/projects/04.jpg';
-import project04Big from './../img/projects/04-big.jpg';
-
-import project05 from './../img/projects/05.jpg';
-import project05Big from './../img/projects/05-big.jpg';
-
-import project06 from './../img/projects/06.jpg';
-import project06Big from './../img/projects/06-big.jpg';
+// import project03 from './../img/projects/03.jpg';
+// import project03Big from './../img/projects/03-big.jpg';
+//
+// import project04 from './../img/projects/04.jpg';
+// import project04Big from './../img/projects/04-big.jpg';
+//
+// import project05 from './../img/projects/05.jpg';
+// import project05Big from './../img/projects/05-big.jpg';
+//
+// import project06 from './../img/projects/06.jpg';
+// import project06Big from './../img/projects/06-big.jpg';
 
 
 const projects = [
@@ -32,32 +32,32 @@ const projects = [
     imgBig: project02Big,
     githubLink: 'https://hangman-orcin.vercel.app/'
   },
-  {
-    title: 'Video portal',
-    skills: 'Vue, Pinia',
-    img: project03,
-    imgBig: project03Big,
-    githubLink: 'https://github.com'
-  },
-  {
-    title: 'Dating app',
-    skills: 'Vue, Typescript',
-    img: project04,
-    imgBig: project04Big,
-    githubLink: 'https://github.com'
-  },
-  {
-    title: 'Landing',
-    skills: 'React, Node.js, Typescript',
-    img: project05,
-    imgBig: project05Big,
-  },
-  {
-    title: 'Gaming community',
-    skills: 'React, Node.js, Typescript',
-    img: project06,
-    imgBig: project06Big,
-  },
+  // {
+  //   title: 'Video portal',
+  //   skills: 'Vue, Pinia',
+  //   img: project03,
+  //   imgBig: project03Big,
+  //   githubLink: 'https://github.com'
+  // },
+  // {
+  //   title: 'Dating app',
+  //   skills: 'Vue, Typescript',
+  //   img: project04,
+  //   imgBig: project04Big,
+  //   githubLink: 'https://github.com'
+  // },
+  // {
+  //   title: 'Landing',
+  //   skills: 'React, Node.js, Typescript',
+  //   img: project05,
+  //   imgBig: project05Big,
+  // },
+  // {
+  //   title: 'Gaming community',
+  //   skills: 'React, Node.js, Typescript',
+  //   img: project06,
+  //   imgBig: project06Big,
+  // },
 ]
 
 export { projects };
